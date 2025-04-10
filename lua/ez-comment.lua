cb3d02387de9e2be25450ee77a94683fd922c180
yellowcrystalz <jaykaycrystal0914@gmail.com>
2025-04-10 00:24:32 -0500

    test: created table M

--- lua/ez-comment.lua
+++ lua/ez-comment.lua
@@ -1,3 +1,9 @@
-vim.api.nvim_create_user_command("Test", function()
-    print("Test!")
-end, {})
+local M = {}
+
+function M.setup()
+    vim.api.nvim_create_user_command("Test", function()
+        print("Test!")
+    end, {})
+end
+
+return M
