b9e68a297963a065860d3c519f211196c8ec2763
yellowcrystalz <jaykaycrystal0914@gmail.com>
2025-04-14 23:00:44 -0500

    feat: can comment multiple lines in visual mode

--- lua/ez-comment.lua
+++ lua/ez-comment.lua
@@ -3,13 +3,24 @@ local M = {}
 
 function M.setup()
     -- Place Holder Keybind
-    vim.keymap.set({"n"}, "<leader>cc", comment)
+    vim.keymap.set({ "v" }, "<leader>cc", comment)
 end
 
 
 function comment()
-    local row = vim.api.nvim_win_get_cursor(0)[1]
-    vim.api.nvim_buf_set_text(0, row - 1, 0, row - 1, 0, {"-- "})
+    local start_pos = vim.fn.getpos("v")
+    local end_pos = vim.fn.getpos(".")
+
+    local start_line = start_pos[2]
+    local end_line = end_pos[2]
+
+    if start_line > end_line then
+        start_line, end_line = end_line, start_line
+    end
+
+    for i = start_line, end_line do
+        vim.api.nvim_buf_set_text(0, i - 1, 0, i - 1, 0, {"-- "})
+    end
 end
 
 
