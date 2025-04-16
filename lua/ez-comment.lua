b7f7b015626e3df48239bcce4e9a6e0f54c984fd
yellowcrystalz <jaykaycrystal0914@gmail.com>
2025-04-15 19:24:09 -0500

    feat: language checker for c, cpp, python, and lua

--- lua/ez-comment.lua
+++ lua/ez-comment.lua
@@ -1,20 +1,14 @@
 local M = {}
 
 
-function M.setup()
-    -- Place Holder Keybind
-    vim.keymap.set({ "v" }, "<leader>/", ez_comment)
-end
-
-
-local function check_comment()
+local function check_comment(comment_symbol)
     local line_pos = vim.fn.getpos(".")
     local line_num = line_pos[2]
 
     local line = vim.api.nvim_buf_get_lines(0, line_num - 1, line_num, false)[1]
     local subline = line:sub(1, 3)
 
-    if subline == "-- " then
+    if subline == comment_symbol then
         return true
     else
         return false
@@ -22,16 +16,34 @@ local function check_comment()
 end
 
 
-local function comment(start_line, end_line)
+local function check_language()
+    local file_name = vim.api.nvim_buf_get_name(0)
+    local ext = file_name:match("^.+%.([^.]+)$")
+
+    local comment_symbol = ""
+
+    if ext == "c" or ext == "cpp" then
+        comment_symbol = "// "
+    elseif ext == "py" then
+        comment_symbol = "# "
+    elseif ext == "lua" then
+        comment_symbol = "-- "
+    end
+
+    return comment_symbol
+end
+
+
+local function comment(start_line, end_line, comment_symbol)
     for i = start_line, end_line do
-        vim.api.nvim_buf_set_text(0, i - 1, 0, i - 1, 0, {"-- "})
+        vim.api.nvim_buf_set_text(0, i - 1, 0, i - 1, 0, {comment_symbol})
     end
 end
 
 
-local function uncomment(start_line, end_line)
+local function uncomment(start_line, end_line, comment_symbol)
     for i = start_line, end_line do
-        vim.api.nvim_buf_set_text(0, i - 1, 0, i - 1, 3, {""})
+        vim.api.nvim_buf_set_text(0, i - 1, 0, i - 1, #comment_symbol, {""})
     end
 end
 
@@ -47,11 +59,20 @@ function ez_comment()
         start_line, end_line = end_line, start_line
     end
 
-    if check_comment() then
-        uncomment(start_line, end_line)
+    local comment_symbol = check_language()
+
+    if check_comment(comment_symbol) then
+        uncomment(start_line, end_line, comment_symbol)
     else
-        comment(start_line, end_line)
+        comment(start_line, end_line, comment_symbol)
     end
+end
+
+
+function M.setup()
+    -- Place Holder Keybind
+    vim.keymap.set({ "v" }, "<leader>/", ez_comment)
+    vim.keymap.set({ "n" }, "<leader>cc", check_language)
 end
 
 
